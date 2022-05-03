355462f65ca2b939ec7a357135affa50a1997b9b
眷曦 <zhaoshijia@yahoo.com>
2022-05-03 17:16:00 +0800

    Site updated: 2022-05-03 17:15:59

---
+++ dist/music.js
@@ -0,0 +1,19 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "春天继续",
+        artist: '春野杉卉',
+        url: 'http://music.163.com/song/media/outer/url?id=1466683551.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+      },
+      {
+        name: '9420',
+        artist: '麦小兜',
+        url: 'http://up.mcyt.net/?down/45967.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/8.jpg',
+      }
+    ]
+});
